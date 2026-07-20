1b5245a1bf9f0dccc5495282cf996627eb945000
Mario Igkiempor <marioigk@proton.me>
2026-07-20 20:22:55 +0100

    various improvements

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
+mkdir -p build
+
 echo "== Building atlas =="
-odin run vendor/atlas-builder -out:atlas-builder.bin
+odin run vendor/atlas-builder -out:build/atlas-builder.bin
 
 echo "== Building and running game =="
-odin run . -out:shooter.bin
+odin run . -out:build/shooter.bin
